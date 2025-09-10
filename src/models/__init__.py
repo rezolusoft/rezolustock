14f5be9119f6e666fa6e970f87d28bdf0c44cf48
Abiodoun Paraïso <contact@abiodoun.dev>
2025-09-10 10:50:58 +0100

    ✏️ Reformat and update db_initializer using peewee_migrate

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,45 +1,19 @@
-from pathlib import Path
-import logging
-from peewee import SqliteDatabase
-from .category import AkontaCategory
-from .product import AkontaProduct
-from .customer import AkontaCustomer
-from .invoice import AkontaInvoice
-from .sale import AkontaSale, AkontaSaleItem
-from .shop import AkontaShop
-from .transaction import AkontaTransaction
-from extras.store import AkontaStore
+from .category import Category
+from .product import Product
+from .customer import Customer
+from .invoice import Invoice
+from .sale import Sale, SaleItem
+from .shop import Shop
+from .transaction import Transaction
 
-# initialiser la base de donnée
-db_path = Path(__file__).parent.parent.joinpath('db/akonta.db')
-db = SqliteDatabase(db_path)
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
-
-def db_initializer(page):
-
-    store = AkontaStore(page)
-
-    db_init = store.get("db_init")
-
-    tables=[AkontaCategory, 
-            AkontaCustomer, 
-            AkontaProduct, 
-            AkontaInvoice, 
-            AkontaSale, 
-            AkontaSaleItem, 
-            AkontaShop, 
-            AkontaTransaction]
-
-    if not db_init:
-
-        logging.info("##### DB INITIALIZATION #####")
-
-        db.connect() # connection a la db
-        db.create_tables(tables) # creation des tables
-
-        store.set("db_init", True)
-
-        logging.info("##### DB INITIALIZED SUCESSFULLY #####")
-    else:
-        logging.info("##### DB ALREADY INITIALIZED #####")
+__all__ = [
+    "Category",
+    "Customer",
+    "Product",
+    "Invoice",
+    "Sale",
+    "SaleItem",
+    "Shop",
+    "Transaction",
+]
---
+++ src/models/db_initializer.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import logging
+from peewee import SqliteDatabase
+from peewee_migrate import Router
+
+# Ton fichier SQLite
+db_path = Path(__file__).parent.parent.joinpath("db/app.db")
+db = SqliteDatabase(db_path)
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+
+def db_initializer():
+    logging.info("##### DB MIGRATIONS START #####")
+    router = Router(db, migrate_dir="src/migrations")
+    router.run() 
+    logging.info("##### DB MIGRATIONS DONE #####")
