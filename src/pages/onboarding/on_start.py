e8213493199fc3f728ae687fe3317c758cfe6282
Abiodoun Paraïso <contact@abiodoun.dev>
2025-09-14 22:56:29 +0100

    ✏️ add onboarding start page

---
+++ src/pages/onboarding/on_start.py
@@ -0,0 +1,54 @@
+import flet as ft
+
+
+def on_start(page) -> ft.Control:
+
+
+
+    next_button = ft.Button(
+                    content=ft.Row(
+                          [   
+                              ft.Text("Commencer"),
+                              ft.Icon(name=ft.Icons.ARROW_FORWARD_ROUNDED, color=ft.Colors.ON_SURFACE),
+                              
+                          ]
+                    ), 
+                      style=ft.ButtonStyle(
+                          shape=ft.RoundedRectangleBorder(10),
+                          padding=15,
+                          bgcolor=ft.Colors.SECONDARY,
+                          text_style=ft.TextStyle(
+                              font_family="PoppinsBold",
+                              size=22
+                          )
+                          ),
+                          color=ft.Colors.ON_SURFACE,
+                        
+                        on_click=lambda e:page.go("/on_shop_register"),
+                        
+                    )
+    
+
+    on_start = ft.Container(ft.Column(
+        expand=True,
+        controls=[
+            ft.Text("🚀", size=40),
+            ft.Text("Démarrer maintenant !", size=40, font_family="PoppinsBold", color=ft.Colors.ON_SURFACE),
+            ft.Text(
+                "Configurer votre boutique et Ajoutez " \
+                "vos premiers produits puis découvrez " \
+                "tout ce que Akonta peut faire pour vous.", size=22, font_family="PoppinsMedium", color=ft.Colors.ON_SURFACE),
+            ft.Container(margin=ft.margin.symmetric(vertical=8)),
+            ft.Row(
+                controls=[
+                    next_button
+                ],
+            )
+        ],
+        alignment=ft.MainAxisAlignment.CENTER
+    ),
+    padding=ft.padding.only(right=20)
+
+    )
+
+    return on_start
