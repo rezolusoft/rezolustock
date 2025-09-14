bafd2241a19e0b1aeef01f4f28062bff1b3ac48b
Abiodoun Paraïso <contact@abiodoun.dev>
2025-09-14 22:53:54 +0100

    ✏️ add onboarding routes

--- src/extras/routes.py
+++ src/extras/routes.py
@@ -5,5 +5,14 @@ routes = [
     '/product',
 
     # Onboarding Pages
-    '/on_welcome'
+    '/on_welcome',
+    '/on_product',
+    '/on_sale',
+    '/on_stats',
+    '/on_start',
+    '/on_shop_register',
+    '/on_add_password',
+    '/on_add_category',
+    '/on_add_product',
+    '/on_done',
 ]
