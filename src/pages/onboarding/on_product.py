e48c7d7fe5f19d488bc7a601d3dd1eb48b0d6a1b
Abiodoun Paraïso <contact@abiodoun.dev>
2025-10-29 10:40:32 +0100

    ✏️ change textsize

--- src/pages/onboarding/on_product.py
+++ src/pages/onboarding/on_product.py
@@ -9,10 +9,10 @@ def on_product(page) -> ft.Control:
         expand=True,
         controls=[
             ft.Text("📦", size=40),
-            ft.Text("Ajoutez vos produits en un clic", size=40, font_family="PoppinsBold", color=ft.Colors.ON_SURFACE),
+            ft.Text("Ajoutez vos produits en un clic", size=25, font_family="PoppinsBold", color=ft.Colors.ON_SURFACE),
             ft.Text(
                 "Centralisez toutes vos références et " \
-                "suivez vos quantités en temps réel.", size=22, font_family="PoppinsMedium", color=ft.Colors.ON_SURFACE),
+                "suivez vos quantités en temps réel.", size=20, font_family="Poppins", color=ft.Colors.ON_SURFACE),
             ft.Container(margin=ft.margin.symmetric(vertical=8)),
             ft.Row(
                 controls=[
