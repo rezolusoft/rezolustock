43b5eeebe234f861ab6bdb0284620894b10342c5
Abiodoun Paraïso <contact@abiodoun.dev>
2025-10-29 10:40:58 +0100

    ✏️ change text size

--- src/pages/onboarding/on_sale.py
+++ src/pages/onboarding/on_sale.py
@@ -8,9 +8,9 @@ def on_sale(page) -> ft.Control:
         expand=True,
         controls=[
             ft.Text("💰", size=40),
-            ft.Text("Gardez le contrôle sur vos transactions", size=40, font_family="PoppinsBold", color=ft.Colors.ON_SURFACE),
+            ft.Text("Gardez le contrôle sur vos transactions", size=25, font_family="PoppinsBold", color=ft.Colors.ON_SURFACE),
             ft.Text(
-                "Enregistrez vos ventes, suivez vos marges et vos bénéfices.", size=22, font_family="PoppinsMedium", color=ft.Colors.ON_SURFACE),
+                "Enregistrez vos ventes, suivez vos marges et vos bénéfices.", size=20, font_family="Poppins", color=ft.Colors.ON_SURFACE),
             ft.Container(margin=ft.margin.symmetric(vertical=8)),
             ft.Row(
                 controls=[
