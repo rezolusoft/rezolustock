55f81df9ed168ca03f39a5fa04cccd291bf26846
Abiodoun Paraïso <contact@abiodoun.dev>
2025-11-17 00:42:00 +0100

    ✏️ update shop model

--- src/models/shop.py
+++ src/models/shop.py
@@ -5,8 +5,8 @@ from models.rezolustock import RstockObject
 class Shop(RstockObject):
     name = CharField(max_length=50)
     logo = CharField(max_length=500, null=True)
-    email = CharField(max_length=50)
-    phone = CharField(max_length=20)
+    email = CharField(max_length=100, null=False, unique=True, index=True)
+    phone = CharField(max_length=20, null=False, unique=True, index=True)
     adress = CharField(max_length=150, null=True)
     balance = DecimalField(decimal_places=2, default=0)
     ifu = CharField(max_length=100, null=True)
