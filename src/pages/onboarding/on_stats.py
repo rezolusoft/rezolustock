95f7e7e8e42455b91e03b6cac0ffa922a86d2f4b
Abiodoun Paraïso <contact@abiodoun.dev>
2026-03-20 03:18:41 +0100

    ✏️ update onboarder stats page

--- src/pages/onboarding/on_stats.py
+++ src/pages/onboarding/on_stats.py
@@ -9,10 +9,10 @@ def on_stats(page) -> ft.Control:
         expand=True,
         controls=[
             ft.Text("📊", size=40),
-            ft.Text("Analysez vos performances", size=25, font_family="PoppinsBold", color=ft.Colors.ON_SURFACE),
+            ft.Text("Analysez vos performances", size=20, font_family="PoppinsBold", color=ft.Colors.ON_SURFACE),
             ft.Text(
-                "Prenez de meilleures décisions grâce à des rapports clairs et précis.", size=20, font_family="Poppins", color=ft.Colors.ON_SURFACE),
-            ft.Container(margin=ft.margin.symmetric(vertical=8)),
+                "Prenez de meilleures décisions grâce à des rapports clairs et précis.", size=15, font_family="Poppins", color=ft.Colors.ON_SURFACE),
+            ft.Container(margin=ft.margin.symmetric(vertical=3)),
             ft.Row(
                 controls=[
                     previous(page=page, route="/on_sale"),
