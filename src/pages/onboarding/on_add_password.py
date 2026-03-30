91d8dc187b5252a56239821e1b739cf6ac587c0e
Abiodoun Paraïso <contact@abiodoun.dev>
2026-03-30 13:09:31 +0100

    ✏️ rewrite user registration logic, improve logic to fit with flet 0.82.2 specs and Add the auto login feature

--- src/pages/onboarding/on_add_password.py
+++ src/pages/onboarding/on_add_password.py
@@ -4,6 +4,8 @@ from extras.store import RStockStore
 from components.rstocknotif import rstocknotif
 from extras.tools import validate_password
 from argon2 import PasswordHasher
+from datetime import datetime
+import asyncio
 from models.shop import Shop
 from models.user import User
 from extras.enums import AccountTypeEnums
@@ -12,13 +14,13 @@ from extras.enums import AccountTypeEnums
 
 def on_add_password(page) -> ft.Control:
 
-    store = RStockStore(page=page)
+    store = RStockStore()
 
-    password_field = ft.TextField(hint_text="*Entrer votre mot de passe", border_radius=10, password=True, can_reveal_password=True)
-    password_confirm_field = ft.TextField(hint_text="*Confirmer votre mot de passe", border_radius=10, password=True, can_reveal_password=True)
+    password_field = ft.TextField(hint_text="*Entrer votre mot de passe", border_radius=10, password=True, can_reveal_password=True, expand=1)
+    password_confirm_field = ft.TextField(hint_text="*Confirmer votre mot de passe", border_radius=10, password=True, can_reveal_password=True, expand=1)
 
     async def set_shop_user(password):
-        shop = store.get("shop_infos")
+        shop = await store.get_shop()
         shop_name = shop["name"]
         shop_email = shop["email"]
         shop_phone = shop["phone"]
@@ -56,17 +58,39 @@ def on_add_password(page) -> ft.Control:
         
         notif = rstocknotif("Opération réussie ✅", "Votre compte administrateur à été crée avec succès !", [])
 
-        store.destroy("shop_infos")
-        store.destroy("onboarding_step")
-        store.set("onboarding_step", "on_add_category")
+        await store.set_onboarding_step("on_add_category")
+
+        #login process
+        # TODO : Centralize this logic in a complet authentication class including registration + base_user_creation + permissions
+        user = User.get(User.email == shop_email)
+        user.last_seen=datetime.now()
+        user.save()
+        user=user.__data__.copy()
+
+        shop_data = Shop.get(Shop.email==shop_email).__data__.copy()
+
+        # quick fix for login
+        user.pop("password", None)
+        user.pop("created_at", None)
+        user.pop("updated_at", None)
+        user.pop("deleted", None)
+
+        shop_data.pop("created_at", None)
+        shop_data.pop("updated_at", None)
+        shop_data.pop("deleted", None)
+
+        user["shop"] = shop_data
+        await store.set_user_data(user)
+        await store.clear_shop()
+        await store.set_onboarding_step("on_add_category")
         
-        page.open(notif)
-        time.sleep(3)
-        page.close(notif)
+        page.show_dialog(notif)
+        await asyncio.sleep(3.5)
+        page.pop_dialog()
         await page.push_route("/on_add_category")
         
     
-    def form_handler(e):
+    async def form_handler(e):
 
         password = password_field.value.strip()
         password_confirm = password_confirm_field.value.strip()
@@ -74,29 +98,29 @@ def on_add_password(page) -> ft.Control:
         password_is_valid, errors = validate_password(password, 4, 128, False, False, False, False, True)
 
         if password_is_valid:
-            password_field.error_text = None
+            password_field.error = None
             if password==password_confirm:
-                password_field.error_text = None
-                password_confirm_field.error_text = None
-                set_shop_user(password=password)
+                password_field.error = None
+                password_confirm_field.error = None
+                await set_shop_user(password=password)
 
             else:
-                password_field.error_text = "Erreur ! Le mots de passes ne correspondent pas"
-                password_confirm_field.error_text = "Erreur ! Le mots de passes ne correspondent pas"
+                password_field.error = "Erreur ! Le mots de passes ne correspondent pas"
+                password_confirm_field.error = "Erreur ! Le mots de passes ne correspondent pas"
 
         else:
             error = ""
             for e in errors:
                 error+=f"{e}\n"
 
-            password_field.error_text = error
+            password_field.error = error
         
         page.update()
 
-    save_user_info = ft.ElevatedButton(
+    save_user_info = ft.Button(
         "Enregistrer Mot de Passe",
         style=ft.ButtonStyle(
-            shape=ft.RoundedRectangleBorder(5),
+            shape=ft.RoundedRectangleBorder(radius=5),
             padding=ft.Padding.symmetric(vertical=15),
             bgcolor=ft.Colors.SECONDARY,
             text_style=ft.TextStyle(
