85755cceac2d047063ff880719dac816e8e7609c
Abiodoun Paraïso <contact@abiodoun.dev>
2026-03-30 13:04:23 +0100

    ✏️ improve and finx onboarding previous and next buttons

--- src/layout/components/onboarder/previous.py
+++ src/layout/components/onboarder/previous.py
@@ -2,25 +2,24 @@ import flet as ft
 from extras.routes import push
 
 
-def previous( route, name="Précédent"):
+def previous(route, name="Précédent"):
     
-    return ft.ElevatedButton(
-                      content=ft.Row(
-                          [
-                              ft.Icon(name=ft.Icons.ARROW_BACK_ROUNDED, color=ft.Colors.WHITE),
-                              ft.Text(name)
-                          ]
-                      ), 
+    return ft.Button(
+                      content=name,
+                      icon=ft.Icons.ARROW_BACK_ROUNDED,
+                      icon_color=ft.Colors.WHITE,
+                      color=ft.Colors.WHITE,
+                      bgcolor=ft.Colors.SECONDARY,
                       style=ft.ButtonStyle(
-                          shape=ft.RoundedRectangleBorder(5),
+                          shape=ft.RoundedRectangleBorder(radius=5),
                           padding=10,
-                          bgcolor=ft.Colors.SECONDARY,
                           text_style=ft.TextStyle(
                               font_family="PoppinsMedium",
-                              size=15
+                              size=15,
+                              
                           )
                           ),
-                          color=ft.Colors.WHITE,
+                          
                         
                         on_click=lambda e:e.page.run_task(push, e.page, route),
                         
