ab5944dea8fea12a8d1a2f894bc95ca49cdd5ca8
Abiodoun Paraïso <contact@abiodoun.dev>
2026-06-09 14:33:33 +0100

    ✏️ update main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,5 @@
 import flet as ft
 from themes import light_theme, dark_theme, font_loader
-from layout import pager, onboarder
-from pages.auth.login import login
-from core.routes import routes
-from importlib import import_module
 from models.db_initializer import db_initializer
 from core.store import RStockStore
 from core.state import RstockState
@@ -16,13 +12,16 @@ async def main(page: ft.Page):
   
     # Initialiser une instance du store
     store = RStockStore()
+    
     # Initialiser une instance du state
     state = RstockState(page)
+    
     # Initialiser une instance du routeur
     onboarding_step = await store.get_onboarding_step()
-    router_engine = RouterEngine(page, store, state)
+    print(f"step------>{onboarding_step}")
+    print(f"route----->{page.route}")
 
-
+    
 
     db_initializer()
 
@@ -33,20 +32,14 @@ async def main(page: ft.Page):
 
     # Charger les police personnalisé 
     font_loader(page=page)
-
+    # Maximiser la fenetre
+    page.window.maximized = True
     # Charger le theme
     page.theme = light_theme
     page.theme_mode = ft.ThemeMode.LIGHT
     page.dark_theme = dark_theme
-    # page.bgcolor = None
-    # page.padding = 0
-    # page.spacing = 0
+   
 
-
-
-
-    # Maximiser la fenetre
-    page.window.maximized = True
 
     # Recuperer les infos de l'onboarding
 
@@ -54,15 +47,24 @@ async def main(page: ft.Page):
     layout = ft.Container(expand=True)
     content_container = ft.Container(expand=True)
 
+
     async def router(e: ft.RouteChangeEvent):
         route = page.route
+        router_engine = RouterEngine(page, store, state)
         result = await router_engine.resolve(route)
 
         if "redirect" in result:
+            print(f"redirect----->{result["redirect"]}")
             await page.push_route(result["redirect"])
             return
-        layout.content = build_layout(page, result, content_container)
+
+        new_content = build_layout(page, result, content_container)
+        layout.content = new_content
         page.update()
+
+        print("new_content =", new_content)
+        print(f"route----->{page.route}")
+        
         
 
 
@@ -70,31 +72,9 @@ async def main(page: ft.Page):
     page.add(layout)
 
     page.on_route_change = router
+
+    page.update()
+
     
-    # Controle de la page par défaut
-    # redirection conditionnel vers la page initiale 
-    if page.route == "/":
-        if onboarding_step=="completed":
-            # add auth verification
-            if state.is_authenticated():
-                await page.push_route('/dashboard')
-            else:
-                saved_session = await store.remember_me()
-                if saved_session:
-                    from core.auth import RstockAuthentication
-                    auth = RstockAuthentication(state, store)
-                    await auth.auto_login()
-                    await page.push_route('/dashboard')
-                else:
-                    await page.push_route('/login')
-        else:
-            if onboarding_step:
-                await page.push_route(f"/{onboarding_step}")
-            else:
-                await page.push_route('/on_welcome')
-
-    else:
-        await page.push_route(page.route)
-
 
 ft.run(main)
