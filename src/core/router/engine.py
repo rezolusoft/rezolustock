ccb811264153023ae7843b93f7384eb8f3d83c9d
Abiodoun Paraïso <contact@abiodoun.dev>
2026-06-09 14:37:54 +0100

    ✏️ add the route switch logic

--- src/core/router/engine.py
+++ src/core/router/engine.py
@@ -10,34 +10,42 @@ class RouterEngine():
 
     async def resolve(self, path: str)->dict:
         route = routes.get(path)
-        
-
-
-        if not route:
-            return {"type":"error", "content": "Page introuvable"}
-        
-
-        # S'assurer du fait que l'utilisateur a fait le onboarding
         onbaording_step = await self.store.get_onboarding_step()
+        
 
-        if onbaording_step != "completed" and route.layout != "onboarding":
-            return {"redirect":f"/{onbaording_step or "on_welcome"}"}
-        
-        
-        # Verifier l'authentification
-        if route.requires_auth and not self.state.is_authenticated():
-            return {"redirect": "/login"}
-        
-        
-        
-        # Chargement de la view...
-        view = route.get_view()
-        content = view()
+        if path == "/":
+            if onbaording_step == "completed":
+                authenticated = await self.state.is_authenticated()
+                if authenticated :
+                    return {"redirect": "/dashboard"}
+                else:
+                    saved_session = await self.store.remember_me()
+                    if saved_session:
+                        from core.auth import RstockAuthentication
+                        auth = RstockAuthentication(self.state, self.store)
+                        await auth.auto_login()
+                        return {"redirect": "/dashboard"}
+                    else:
+                        return {"redirect": "/login"}
 
-        
-        return {
-            "type" : "view",
-            "layout" : route.layout,
-            "content" : content
-        }
+            else:
+                return {"redirect": f"/{onbaording_step or 'on_welcome'}"}
+
+        else:
+            
+            if route :
+                # Chargement de la view...
+                view = route.get_view()
+    
+                content = view()
+    
+                if route.requires_root:
+                    content = view(page=self.page)
+    
+            
+                return {
+                    "type" : "view",
+                    "layout" : route.layout,
+                    "content" : content
+                }
 
