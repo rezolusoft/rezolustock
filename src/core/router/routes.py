a2c8f910c5701132e1cf2643a096f927b85f38c1
Abiodoun Paraïso <contact@abiodoun.dev>
2026-06-09 14:38:11 +0100

    ✏️ update routes

--- src/core/router/routes.py
+++ src/core/router/routes.py
@@ -7,22 +7,20 @@ routes = {
     "/login": Route(path="/login", module="pages.auth.login", view="login", layout="auth"),
 
     # Main
-    "/dashboard": Route(path="/dashboard", module="pages.dashboard", view="dashboard"),
-    "/inventory": Route(path="/inventory", module="pages.inventory", view="inventory"),
-    "/product": Route(path="/product", module="pages.product", view="product"),
+    "/dashboard": Route(path="/dashboard", module="pages.dashboard", view="dashboard", layout="main"),
+    "/inventory": Route(path="/inventory", module="pages.inventory", view="inventory", layout="main"),
+    "/product": Route(path="/product", module="pages.product", view="product", layout="main"),
 
     # Onboarding
-    "/on_welcome": Route(path="/on_welcome", module="pages.onboarding.on_welcome", view="on_welcome"),
-    "/on_product": Route(path="/on_product", module="pages.onboarding.on_product", view="on_product"),
-    "/on_sale": Route(path="/on_sale", module="pages.onboarding.on_sale", view="on_sale"),
-    "/on_stats": Route(path="/on_stats", module="pages.onboarding.on_stats", view="on_stats"),
-    "/on_start": Route(path="/on_start", module="pages.onboarding.on_start", view="on_start"),
-    "/on_shop_register": Route(path="/on_shop_register", module="pages.onboarding.on_shop_register", view="on_shop_register"),
-    "/on_add_password": Route(path="/on_add_password", module="pages.onboarding.on_add_password", view="on_add_password"),
-    "/on_add_category": Route(path="/on_add_category", module="pages.onboarding.on_add_category", view="on_add_category"),
-    "/on_add_product": Route(path="/on_add_product", module="pages.onboarding.on_add_product", view="on_add_product"),
-    "/on_done": Route(path="/on_done", module="pages.onboarding.on_done", view="on_done"),
-
+    "/on_welcome": Route(path="/on_welcome", module="pages.onboarding.on_welcome", view="on_welcome",  layout="onboarding"),
+    "/on_product": Route(path="/on_product", module="pages.onboarding.on_product", view="on_product",  layout="onboarding"),
+    "/on_sale": Route(path="/on_sale", module="pages.onboarding.on_sale", view="on_sale",  layout="onboarding"),
+    "/on_stats": Route(path="/on_stats", module="pages.onboarding.on_stats", view="on_stats",  layout="onboarding"),
+    "/on_start": Route(path="/on_start", module="pages.onboarding.on_start", view="on_start",  layout="onboarding"),
+    "/on_shop_register": Route(path="/on_shop_register", module="pages.onboarding.on_shop_register", view="on_shop_register", requires_root=True,  layout="onboarding"),
+    "/on_add_password": Route(path="/on_add_password", module="pages.onboarding.on_add_password", view="on_add_password", requires_root=True,  layout="onboarding"),
+    "/on_add_category": Route(path="/on_add_category", module="pages.onboarding.on_add_category", view="on_add_category", requires_root=True,  layout="onboarding"),
+    "/on_add_product": Route(path="/on_add_product", module="pages.onboarding.on_add_product", view="on_add_product", requires_root=True,  layout="onboarding"),
+    "/on_done": Route(path="/on_done", module="pages.onboarding.on_done", view="on_done",  layout="onboarding"),
 
 }
-
