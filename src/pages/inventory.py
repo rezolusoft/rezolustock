b8ffd8ad4341e10352f4cd3a2baaa235877a4c88
Abiodoun Paraïso <contact@abiodoun.dev>
2026-08-03 16:13:06 +0100

    ✏️ add context to views

--- src/pages/inventory.py
+++ src/pages/inventory.py
@@ -1,5 +1,5 @@
 import flet as ft
 
-def inventory()->ft.Control :
+def inventory(page)->ft.Control :
     inventory = ft.Container(content=ft.Text("Inventory -> Contenu Principal"), bgcolor=ft.Colors.SURFACE)
     return inventory
