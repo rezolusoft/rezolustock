a178fe1d67d2f7d60baf81c03c12e981c200ef02
Abiodoun Paraïso <contact@abiodoun.dev>
2026-08-10 19:09:09 +0100

    ✏️ update active link effect 🖌️add side menu footer

--- src/layout/components/pager/side_menu.py
+++ src/layout/components/pager/side_menu.py
@@ -9,37 +9,30 @@ def side_menu_button(title, icon, page=None, destination=None)->ft.Control:
     is_active = (page.route == destination)
 
     # Couleurs
-    default_bg = ft.Colors.TRANSPARENT
-    hover_bg = ft.Colors.ORANGE_50
-    active_bg = ft.Colors.ORANGE_100
+    default_color = ft.Colors.PRIMARY
+    active_color = ft.Colors.SECONDARY
 
     # On part avec la couleur active ou par défaut
-    bg_color = active_bg if is_active else default_bg
+    color = active_color if is_active else default_color
 
     button = ft.Container(
                 ft.Row(
                     controls=[
-                        ft.Icon(icon=getattr(ft.Icons, icon), color=ft.Colors.PRIMARY, size=20),
-                        ft.Text(title, color=ft.Colors.ON_SURFACE),
+                        ft.Icon(icon=getattr(ft.Icons, icon), color=color, size=20),
+                        ft.Text(title, color=color),
                     
                     ],
                   expand=True,  
                 ),
-                padding=ft.Padding.all(3),
-                margin=ft.Margin.only(left=7),
-                bgcolor=bg_color,
+                padding=ft.Padding.all(2),
+                margin=ft.Margin.only(left=8),
                 on_click= lambda e : e.page.run_task(push, e.page, destination),
-                border_radius=ft.BorderRadius.all(5),
+                border_radius=ft.BorderRadius.all(10),
 
                 
             )
     
-    def on_hover(e: ft.HoverEvent):
-        if not is_active:  # On ne change pas si déjà actif
-            button.bgcolor = hover_bg if e.data == "true" else default_bg
-            button.update()
 
-    button.on_hover = on_hover
     
     return button
 
@@ -64,52 +57,53 @@ spacer.margin = ft.Margin.symmetric(vertical=10)
 def side_menu(page)->ft.Control:
     
     side_menu = ft.Container(
-     # bgcolor=ft.Colors.SURFACE,
+     bgcolor=ft.Colors.SURFACE,
      width=240,
      padding=10,
-     border_radius=ft.BorderRadius.all(7),
-    content=ft.Column(
-        expand=True,
-        scroll=ft.ScrollMode.AUTO,
-        alignment=ft.MainAxisAlignment.START,
-        horizontal_alignment=ft.CrossAxisAlignment.START,
-        controls=[
-            logo,
-           
-            menu_title('Catalogue'),
-            side_menu_button(title="Produits", icon="INVENTORY_2_OUTLINED", destination="/product", page=page),
-            side_menu_button(title="Catégories", icon="CATEGORY_OUTLINED", destination="/category", page=page),
+     border_radius=ft.BorderRadius.all(10),
+     content= ft.Column(
+         controls=[
+            ft.Column(
+                        expand=True,
+                        scroll=ft.ScrollMode.AUTO,
+                        alignment=ft.MainAxisAlignment.START,
+                        horizontal_alignment=ft.CrossAxisAlignment.START,
+                        controls=[
+                                    logo,
+                                
+                                    menu_title('Catalogue'),
+                                    side_menu_button(title="Produits", icon="INVENTORY_2_OUTLINED", destination="/product", page=page),
+                                    side_menu_button(title="Catégories", icon="CATEGORY_OUTLINED", destination="/category", page=page),
 
-            menu_title('Stock'),
-            side_menu_button(title="Mon stock", icon="INVENTORY_OUTLINED", destination="/inventory", page=page),
-            
-            menu_title("Ventes & Finances"),
-            side_menu_button(title="Transactions", icon="POINT_OF_SALE_OUTLINED", destination="/transactions", page=page),
-            side_menu_button(title="Facturation", icon="RECEIPT_LONG_OUTLINED", destination="/invoices", page=page),
-            side_menu_button(title="Dépenses", icon="MONEY_OFF_OUTLINED", destination="/expenses", page=page),
+                                    menu_title('Stock'),
+                                    side_menu_button(title="Mon stock", icon="INVENTORY_OUTLINED", destination="/inventory", page=page),
+                                    
+                                    menu_title("Ventes & Finances"),
+                                    side_menu_button(title="Transactions", icon="POINT_OF_SALE_OUTLINED", destination="/transactions", page=page),
+                                    side_menu_button(title="Facturation", icon="RECEIPT_LONG_OUTLINED", destination="/invoices", page=page),
+                                    side_menu_button(title="Dépenses", icon="MONEY_OFF_OUTLINED", destination="/expenses", page=page),
 
-            menu_title("Rapports"),
-            side_menu_button(title="Ventes", icon="DATA_EXPLORATION_OUTLINED", destination="/sale_report", page=page),
-            side_menu_button(title="Inventaire", icon="TABLE_CHART_OUTLINED", destination="/inventory_report", page=page),
-            side_menu_button(title="Bénéfices & Pertes", icon="BUBBLE_CHART_OUTLINED", destination="/profit_loss", page=page),
+                                    menu_title("Rapports"),
+                                    side_menu_button(title="Ventes", icon="DATA_EXPLORATION_OUTLINED", destination="/sale_report", page=page),
+                                    side_menu_button(title="Inventaire", icon="TABLE_CHART_OUTLINED", destination="/inventory_report", page=page),
+                                    side_menu_button(title="Bénéfices & Pertes", icon="BUBBLE_CHART_OUTLINED", destination="/profit_loss", page=page),
 
-            menu_title("Paramètres"),
-            side_menu_button(title="Devise", icon="CURRENCY_EXCHANGE_OUTLINED", destination="/currency", page=page),
-            side_menu_button(title="Unités de mesure", icon="SQUARE_FOOT_OUTLINED", destination="/units", page=page),
-            side_menu_button(title="Utilisateurs", icon="PERSON_4_OUTLINED", destination="/feedback", page=page),
-            side_menu_button(title="Permissions", icon="SUPERVISED_USER_CIRCLE_OUTLINED", destination="/support", page=page),
+                                    menu_title("Paramètres"),
+                                    side_menu_button(title="Devise", icon="CURRENCY_EXCHANGE_OUTLINED", destination="/currency", page=page),
+                                    side_menu_button(title="Unités de mesure", icon="SQUARE_FOOT_OUTLINED", destination="/units", page=page),
+                                    side_menu_button(title="Utilisateurs", icon="PERSON_4_OUTLINED", destination="/feedback", page=page),
+                                    side_menu_button(title="Permissions", icon="SUPERVISED_USER_CIRCLE_OUTLINED", destination="/support", page=page),
 
-            menu_title("Centre d'aide"),
-            side_menu_button(title="Documentation", icon="MENU_BOOK_OUTLINED", destination="/feedback", page=page),
-            side_menu_button(title="Support Client", icon="SUPPORT_AGENT_ROUNDED", destination="/support", page=page),
+                                    menu_title("Centre d'aide"),
+                                    side_menu_button(title="Documentation", icon="MENU_BOOK_OUTLINED", destination="/feedback", page=page),
+                                    side_menu_button(title="Support Client", icon="SUPPORT_AGENT_ROUNDED", destination="/support", page=page),
+                                ],
+                    ),
 
+         ft.Container(ft.Text("Built With ❤️ By Rezolusoft", color=ft.Colors.PRIMARY, font_family="PoppinsSemiBold", text_align=ft.TextAlign.CENTER), width=float("inf"), alignment=ft.Alignment.CENTER, padding=ft.Padding.symmetric(vertical=20, horizontal=10), bgcolor=ft.Colors.GREY_100, border_radius=10)
 
-        ],
-        
-
-        ),
-
-        
+         ]
+     )
 
 )
 
