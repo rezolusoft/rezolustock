5257e33a12a16d439c49e1f53647b0c71cd7c04a
Abiodoun Paraïso <contact@abiodoun.dev>
2026-08-11 23:59:30 +0100

    ✏️ update cateory page

--- src/pages/category/category.py
+++ src/pages/category/category.py
@@ -1,6 +1,23 @@
 import flet as ft
+from components.category import category_list 
+from components.shared import new_button, export_to_pdf, export_to_xls
+from core.context.state import RstockState
+from core.auth.permissions import ADD_CATEGORY
 
 def category(page)->ft.Control :
+
+
+    # load context
+    
+    state = RstockState(page)
+
+    # new category add button
+    add = new_button()
+    add.visible = state.can(ADD_CATEGORY)
+
+    # boutton d'actiong généraux
+    pdf = export_to_pdf()
+    xls = export_to_xls()
 
     category = ft.Container(
             padding=10,
@@ -8,20 +25,27 @@ def category(page)->ft.Control :
                 controls=[
                     ft.Container(
                         ft.Row(
+                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            expand=1,
                             controls=[
                                 ft.Column(
                                     controls=[
-                                        ft.Text("Catégories"),
-                                        ft.Text("Gérez vos catégories ici")
+                                        ft.Text("Catégories", color=ft.Colors.PRIMARY, font_family="PoppinsBold"),
+                                        ft.Text("Gérez vos catégories ici", color=ft.Colors.PRIMARY, font_family="PoppinsMedium")
                                     ]
-                                )
+                                ),
+                                ft.Row(
+                                        [
+                                            add,
+                                            pdf,
+                                            xls
+                                        ]
+                                    )
                             ]
-                        )
+                        ),
+                        padding=10
                     ),
-                    ft.Container(
-                        bgcolor=ft.Colors.SURFACE,
-                        border_radius=10,
-                    )
+                    category_list(page),
                 ]
             )
         )
